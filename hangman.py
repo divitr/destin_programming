61b590e67509084f91a6729b8fd895cfde821bf9
Destiniscool <destindoan3@gmail.com>
2024-06-24 15:50:45 -0700

    hangman

--- hangman.py
+++ hangman.py
@@ -1,10 +1,13 @@
 import random
 fruits = ["mango", "apple", "banana", "orange", "blueberry", "Lime", "Advocado"]
 fruitname = random.choice(fruits)
+list = []
 x = len(fruitname)
 for i in range(x):
-    list.append("")
+    list.append("_")
 for i in range(15):
     guess = input("guess: ")
     if guess in fruitname:
-    
+        list.remove("_")
+        list.append(guess)
+        print(guess)        
