7660458f7c3682578107d77a384f90be00976c72
Destiniscool <destindoan3@gmail.com>
2024-07-22 15:43:32 -0700

    tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -1,17 +1,8 @@
 rows, cols = 3, 3
 Board = [["-"]*cols]*rows
 while True:
-    x = input("player1: ")
-    y = input("player2: ")
-    if x == "0,1":
-        Board[0][0] = "X"
-        print(Board)
-    if x == "0,2":
-        Board[0][1] = "X"
-        print(Board)
-    if x == "0,3":
-        Board[0][2] = "X"
-        print(Board)
-    if x == "1,0":
-        Board[0][0] = "X"
-        print(Board)
+    symbol = input("symbol")
+    row = int(input("row?"))
+    Col = int(input("col?"))
+    Board[row-1][Col-1] = symbol
+    print(Board)
